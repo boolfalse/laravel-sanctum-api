10a99d71338125ab0f5be61b7ef191e1080a9bb6
boolfalse <boolfalse@mail.ru>
2021-07-06 02:39:15 +0400

    Auth Routes (register, login, logout)

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $fields = $request->validate([
+            'name' => 'required|string',
+            'email' => 'required|string|unique:users,email',
+            'password' => 'required|string|confirmed',
+        ]);
+
+        $user = User::create([
+            'name' => $fields['name'],
+            'email' => $fields['email'],
+            'password' => bcrypt($fields['password']),
+        ]);
+
+//        $token = $user->createToken('myapptoken')->plainTextToken;
+
+        return response()->json([
+            'success' => true,
+            'message' => "Successfully registered.",
+//            'user' => $user,
+//            'token' => $token,
+        ], 201);
+    }
+
+    public function login(Request $request)
+    {
+        $fields = $request->validate([
+            'email' => 'required|string',
+            'password' => 'required|string',
+        ]);
+
+        $user = User::where('email', $fields['email'])->first();
+
+        if(!$user || !Hash::check($fields['password'], $user->password)) {
+            return response()->json([
+                'success' => false,
+                'message' => "Bad credentials!",
+            ], 401);
+        }
+
+        $token = $user->createToken('myapptoken')->plainTextToken;
+
+        return response()->json([
+            'success' => true,
+            'message' => "Successfully logged in.",
+            'user' => $user,
+            'token' => $token,
+        ], 201);
+    }
+
+    public function logout(Request $request)
+    {
+        auth()->user()->tokens()->delete();
+
+        return response()->json([
+            'success' => true,
+            'message' => "Logged out.",
+        ], 204);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,14 +1,25 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ProductController;
 
 // Public Routes
-Route::resource('/products', ProductController::class);
+Route::get('/products', [ProductController::class, 'index']);
+Route::get('/products/{id}', [ProductController::class, 'show']);
+Route::get('/products/search/{term}', [ProductController::class, 'search']);
+
+// Auth Routes
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
 
 // Protected Routes
 Route::group([
     'middleware' => ['auth:sanctum'],
 ], function () {
-    Route::get('/products/search/{term}', [ProductController::class, 'search']);
+    Route::post('/products', [ProductController::class, 'store']);
+    Route::put('/products/{id}', [ProductController::class, 'update']);
+    Route::delete('/products/{id}', [ProductController::class, 'destroy']);
+
+    Route::delete('/logout', [AuthController::class, 'logout']);
 });
